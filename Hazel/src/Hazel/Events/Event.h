5b859032f482c65bfd1045a1df023638ce7dc5e6
zhang-yish <3556645295@qq.com>
2025-10-26 23:44:04 +0800

    更新了部分关于构建事件系统的内容

---
+++ Hazel/src/Hazel/Events/Event.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "Hazel/Core.h"
+
+#include <string>
+#include <functional>
+
+namespace Hazel {
+
+	// Events in Hazel are currently blocking, meaning when an event occurs it
+	// immediately gets dispatched and must ve deakt with right then an there.
+	// For the future, a better strategy might be to buffer events in an event
+	// bus and process them during the "evebt" part of the update stage.
+
+	enum class EventType
+	{
+		None = 0,
+		WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+		AppTick, AppUpdate, AppRender,
+		KeyPressed, KeyReleased,
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
+
+	enum EventCategory
+	{
+		None = 0,
+		EventCategoryApplication	= BIT(0),
+		EventCategoryInput			= BIT(1),
+		EventCategoryKeyboard		= BIT(2),
+		EventCategoryMouse			= BIT(3),
+		EventCategoryMouseButton	= BIT(4)
+	};
+
+#define EVEBT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+								virtual EventType GetEventType() const override { return GetStaticType; }\
+								virtual const char* GetName() const overrude { return #type; }
+
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+
+	class HAZEL_API Event
+	{
+		friend class EventDispatcher;
+	public:
+		virtual EventType GetEventType() const = 0;
+		virtual const char* GetName() const = 0;
+		virtual int GetCategoryFlags() const = 0;
+		virtual std::string ToString() const { return GetName(); }
+
+		inline bool IsInCategory(EventCategory category)
+		{
+			return GetCategoryFlags() & category;
+		}
+	protected:
+		bool m_Handled = false;
+	};
+}
